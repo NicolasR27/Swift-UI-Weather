4a0c02c5b844267dbb367b83580fbf2d348e17d7
Nicolas Rios <nickrios33@yahoo.com>
2021-01-21 23:10:08 -0800

    Initial Commit

---
+++ Swift-UI-Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-UI-Weather
+//
+//  Created by Nicolas Rios on 1/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-UI-Weather/Swift_UI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_WeatherApp.swift
+//  Swift-UI-Weather
+//
+//  Created by Nicolas Rios on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
